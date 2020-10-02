80aabca644d8b54f220f89f284a061ee3d7daa4d
ashish khatiwada <ashishkhatiwada212@gmail.com>
2020-10-02 09:01:39 +0545

    Managed Folder Structure for different types of models.

---
+++ Millo/Models/Social/Photo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Millo.Models
+{
+    public class Photo
+    {
+        public int Id { get; set; }
+        public string AlbumId { get; set; }
+        public string Caption { get; set; }
+        public DateTime Created { get; set; }
+        public string LargeSoruce { get; set; }
+        public string Link { get; set; }
+        public string MediumSource { get; set; }
+        public string Owner { get; set; }
+        public string PhotoId { get; set; }
+        public string SmallSource { get; set; }
+    }
+}
